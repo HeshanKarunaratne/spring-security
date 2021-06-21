1727ef8a5a6e06c29df3b8c5082e8fcfb6f0c508
Heshan Karuaratne <heshan.karunaratne@axiatadigitallabs.com>
2021-06-21 20:47:33 +0530

    end of section 3

--- spring-boot-security-course1/src/main/java/com/example/demo/security/ApplicationUserRole.java
+++ spring-boot-security-course1/src/main/java/com/example/demo/security/ApplicationUserRole.java
@@ -1,12 +1,15 @@
 package com.example.demo.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import static com.example.demo.security.ApplicationUserPermission.*;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public enum ApplicationUserRole {
     STUDENT(Sets.newHashSet()),
-    ADMIN(Sets.newHashSet(STUDENT_READ, STUDENT_WRITE, COURSE_READ, COURSE_WRITE));
+    ADMIN(Sets.newHashSet(STUDENT_READ, STUDENT_WRITE, COURSE_READ, COURSE_WRITE)),
+    ADMINTRAINEE(Sets.newHashSet(STUDENT_READ, COURSE_READ));
 
     private Set<ApplicationUserPermission> permissions;
 
@@ -20,5 +23,13 @@ public enum ApplicationUserRole {
 
     public void setPermissions(Set<ApplicationUserPermission> permissions) {
         this.permissions = permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permission = getPermissions().stream()
+                .map(permissions -> new SimpleGrantedAuthority(permissions.getPermission()))
+                .collect(Collectors.toSet());
+        permission.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permission;
     }
 }
